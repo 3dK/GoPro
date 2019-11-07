d9a44eb6f9a5009e1d1884acbe7269ba88993135
3dK <3dK@users.noreply.github.com>
2019-11-07 12:08:15 +0000

    Update gopro.tcl
    Parametrized encoding settings

--- gopro.tcl
+++ gopro.tcl
@@ -1,41 +1,41 @@
 #!/usr/bin/tclsh8.6
 #
-# Author:		Eduard Kulchinsky 
+# Author:	Eduard Kulchinsky 
 #
-# Title:		Sync, join and convert from GoPro device when plugged in USB
+# Title:	Sync, join and convert from GoPro device when plugged in USB
 #
 # Description:	1 - When GoPro is plugged in USB port, UDEV will start a SYSTEMD service (gopro.tcl auto move)
-#				2 - Videos will be moved to local storage (UNPROCESSED_VIDEOS). Message is sent when finished
-#				3 - Videos are joined (JOINED_VIDEOS) on daily timeframe (06:00:00 - 05:59:59 (of next day)) in sequential order
-#				4 - Daily videos (YYYYMMDD.mp4) are transcoded using x264/x265 (CONVERTED_VIDEOS)
+#		2 - Videos will be moved to local storage (UNPROCESSED_VIDEOS). Message is sent when finished
+#		3 - Videos are joined (JOINED_VIDEOS) on daily timeframe (06:00:00 - 05:59:59 (of next day)) in sequential order
+#		4 - Daily videos (YYYYMMDD.mp4) are transcoded using x264/x265 (CONVERTED_VIDEOS)
 #
-# Usage:		Automatic
-#					Next step is called after previous is finished (auto move -> auto join -> auto convert):
-#						Transfer all images and videos from GoPro, join and covert videos: "gopro.tcl auto move" -> auto join -> auto convert
-#						Join all videos and convert: "gopro.tcl auto join" -> auto convert
-#						Convert all videos: "gopro.tcl auto convert"
-#					All files within default directories will be processed: move (UNPROCESSED_VIDEOS), join (JOINED_VIDEOS) and convert (CONVERTED_VIDEOS)
-#				Manual
-#					Only invoked step is performed on all or specified files:
-#						Transfer all images and videos from GOPRO_MNT to UNPROCESSED_VIDEOS: "gopro.tcl manual move"
-#						Join videos to JOINED_VIDEOS: "gopro.tcl manual join [*]"
-#						Convert videos to CONVERTED_VIDEOS: "gopro.tcl manual convert [*]"
-#					[*] If no files are specified, default path is used for input/output and all files are processed
-#					[*] If filenames are specified, only these files from default input path will be processed
-#					[*] If logical/physical path to files is provided, resulting output will be stored in default path
+# Usage:	Automatic
+#			Next step is called after previous is finished (auto move -> auto join -> auto convert):
+#				Transfer all images and videos from GoPro, join and covert videos: "gopro.tcl auto move" -> auto join -> auto convert
+#				Join all videos and convert: "gopro.tcl auto join" -> auto convert
+#				Convert all videos: "gopro.tcl auto convert"
+#			All files within default directories will be processed: move (UNPROCESSED_VIDEOS), join (JOINED_VIDEOS) and convert (CONVERTED_VIDEOS)
+#		Manual
+#			Only invoked step is performed on all or specified files:
+#				Transfer all images and videos from GOPRO_MNT to UNPROCESSED_VIDEOS: "gopro.tcl manual move"
+#				Join videos to JOINED_VIDEOS: "gopro.tcl manual join [*]"
+#				Convert videos to CONVERTED_VIDEOS: "gopro.tcl manual convert [*]"
+#			[*] If no files are specified, default path is used for input/output and all files are processed
+#			[*] If filenames are specified, only these files from default input path will be processed
+#			[*] If logical/physical path to files is provided, resulting output will be stored in default path
 #
 # Instalation:	Enable UDEV rule (99-gopro.rules)
-#				Enable SYSTEMD service (gopro@service)
-#				Create mounting directory for GPHOTOFS
+#		Enable SYSTEMD service (gopro@service)
+#		Create mounting directory for GPHOTOFS
 #
-# Requirements:	GPhotoFS
+# Requirements:	Tcl 8.6
+#		GPhotoFS
 #		FFMpeg
-#		Tcl 8.6
 #
-# To-do:		Parallel service execution not working. While "auto move" service is running, other "auto move" will be ignored
-#				Move converted videos to backup storage with proper ownership/permissions for cloud usage
+# To-do:	Parallel service execution not working. While "auto move" service is running, other "auto move" will be ignored
+#		Move converted videos to backup storage with proper ownership/permissions for cloud usage
+#		Evaluate VP9, AV1, ... codecs for storage and eventual streaming (browser support)
 #
-
 
 # Paths for video processing
 set GOPRO_MNT "/mnt/gopro"
@@ -55,6 +55,12 @@ set FFMPEG [exec which ffmpeg]
 set FFPROBE [exec which ffprobe]
 set GPHOTOFS [exec which gphotofs]
 set FUSERMOUNT [exec which fusermount]
+
+# Video enconding settings (no hardware support)
+# x264: superfast 26
+# x265: ultrafast 28
+# VP9: ?
+set FFMPEG_ENCODE_SETTINGS "-c:a copy -c:v libx264 -preset veryfast -crf 29"
 
 # Push notification settings
 # Get from https://llamalab.com/automate/cloud/
@@ -240,7 +246,7 @@ switch -nocase -- [lindex $argv 1] {
 		}
 		
 		# Send push notification to PUSH_DEVICE
-		pushNotification $PUSH_DEVICE "GoPro can now be disconnected"
+		pushNotification $PUSH_DEVICE "GoPro Can now be disconnected"
 		
 		# When finished, call join script if in auto mode
 		switch -nocase -- [lindex $argv 0] {
@@ -534,7 +540,7 @@ switch -nocase -- [lindex $argv 1] {
 				puts -nonewline "Processing daily video: $videoFile"
 				set outputFile [file join $GOPRO_HOME $CONVERTED_VIDEOS [file tail $videoFile]]
 				
-				set pid [eval exec $NICE $IONICE $FFMPEG -y -i $videoFile -c:a copy -c:v libx264 -preset veryfast -crf 26 $outputFile >>& [regsub {\.mp4$} $outputFile {.log}] &]
+				set pid [eval exec $NICE $IONICE $FFMPEG -y -i $videoFile $FFMPEG_ENCODE_SETTINGS $outputFile >>& [regsub {\.mp4$} $outputFile {.log}] &]
 				
 				# pidList 0: PID 1: Source 2: Output 3: Start Time
 				lappend pidList [list $pid $videoFile $outputFile [clock seconds]]
